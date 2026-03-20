5ffd71c44037b2f04a84eeb7bf2b69d0e7a0341c
alexeller11 <88058746+alexeller11@users.noreply.github.com>
2026-03-20 00:11:29 -0300

    Update TrpcProvider.tsx

--- client/src/providers/TrpcProvider.tsx
+++ client/src/providers/TrpcProvider.tsx
@@ -1,15 +1,9 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
-import { trpc } from "@/lib/trpc";
-import { useState } from "react";
-import superjson from "superjson";
-
 export function TrpcProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      transformer: superjson, // <-- MOVA PARA AQUI (FORA DOS LINKS)
+      transformer: superjson, // <-- Deve ficar aqui no topo
       links: [
         httpBatchLink({
           url: "/api/trpc",
