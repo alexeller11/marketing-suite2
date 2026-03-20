938a8844044c57a64d07a12b260aed930099ec4b
alexeller11 <88058746+alexeller11@users.noreply.github.com>
2026-03-20 15:22:14 -0300

    Update trpc.ts

--- server/_core/trpc.ts
+++ server/_core/trpc.ts
@@ -1,8 +1,12 @@
 import { NOT_ADMIN_ERR_MSG, UNAUTHED_ERR_MSG } from '@shared/const';
 import { initTRPC, TRPCError } from "@trpc/server";
+import superjson from "superjson";
 import type { TrpcContext } from "./context";
 
-const t = initTRPC.context<TrpcContext>().create();
+// ATIVAMOS O SUPERJSON AQUI PARA O BACKEND VOLTAR A ENTENDER O FRONTEND
+const t = initTRPC.context<TrpcContext>().create({
+  transformer: superjson,
+});
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
