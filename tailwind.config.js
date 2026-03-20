d8d9cd1c2b44ca0dfc78c8d89c8e5942a1d1e6cb
alexeller11 <88058746+alexeller11@users.noreply.github.com>
2026-03-19 23:12:56 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [
+    "./client/index.html",
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+}
