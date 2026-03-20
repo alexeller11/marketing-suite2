856ca2d9ca0719262c63b6581f300e417a91d03b
alexeller11 <88058746+alexeller11@users.noreply.github.com>
2026-03-20 09:39:18 -0300

    Update env.ts

--- server/_core/env.ts
+++ server/_core/env.ts
@@ -3,7 +3,6 @@ export const ENV = {
     cookieSecret: process.env.JWT_SECRET ?? "",
     databaseUrl: process.env.DATABASE_URL ?? "",
     isProduction: process.env.NODE_ENV === "production",
-    // Adicione estas linhas para evitar o crash do servidor:
     ownerOpenId: process.env.OWNER_OPEN_ID ?? "",
     ownerName: process.env.OWNER_NAME ?? "",
     forgeApiUrl: process.env.OAUTH_SERVER_URL ?? "https://api.manus.im",
