5cecc43f1dc23a2d971453ddfd233b24e4e27c68
alexeller11 <88058746+alexeller11@users.noreply.github.com>
2026-03-19 22:24:55 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,16 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import Login from "@/pages/Login";
-import Dashboard from "@/pages/Dashboard";
-import Settings from "@/pages/Settings";
-import Clients from "@/pages/Clients";
-import ClientDetail from "@/pages/ClientDetail";
-import Campaigns from "@/pages/Campaigns";
-import Alerts from "@/pages/Alerts";
-import { Route, Switch } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/" component={Dashboard} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/clients" component={Clients} />
-      <Route path="/clients/:id" component={ClientDetail} />
-      <Route path="/campaigns" component={Campaigns} />
-      <Route path="/settings" component={Settings} />
-      <Route path="/alerts" component={Alerts} />
-      <Route path="/404" component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import { TrpcProvider } from "@/providers/TrpcProvider";
 
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider defaultTheme="dark">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
+      <TrpcProvider>
+        <ThemeProvider defaultTheme="dark">
+          <TooltipProvider>
+            <Toaster />
+            <Router />
+          </TooltipProvider>
+        </ThemeProvider>
+      </TrpcProvider>
     </ErrorBoundary>
   );
 }
-
-export default App;
